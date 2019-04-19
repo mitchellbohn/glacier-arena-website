56b9e3107d3e422dfb601858c370462e92c22b40
DustinLep <DustinLeprechaun@gmail.com>
2019-04-19 11:27:20 -0600

    Adjusted Model

--- glacier-ice-arena/HockeyLeague/models.py
+++ glacier-ice-arena/HockeyLeague/models.py
@@ -3,22 +3,26 @@ from cms.models import CMSPlugin
 
 # Create your models here.
 
-class League(CMSPlugin):
-    League_Name = models.CharField(blank=True, max_length=200,)
+class League(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True)
+    league_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.league_name
 
 class Team(models.Model):
     league = models.ForeignKey(League)
     name = models.CharField(blank = True, max_length=200,)
-    team_logo = models.ImageField('Image Path')
-    games_played = models.IntegerField()
-    wins = models.IntegerField()
-    losses = models.IntegerField()
-    ties = models.IntegerField()
-    overtime_losses = models.IntegerField()
-    goals_for = models.IntegerField()
-    goals_against = models.IntegerField()
-    goal_difference = models.IntegerField()
-    PTS = models.IntegerField()
+    team_logo = models.ImageField(upload_to="/team-logos/")
+    games_played = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+    ties = models.IntegerField(default=0)
+    overtime_losses = models.IntegerField(default=0)
+    goals_for = models.IntegerField(default=0)
+    goals_against = models.IntegerField(default=0)
+    goal_difference = models.IntegerField(default=0)
+    PTS = models.IntegerField(default=0)
 
 class Player(models.Model):
     team = models.ForeignKey(Team)
@@ -28,12 +32,15 @@ class Player(models.Model):
 
 class Games(models.Model):
     league = models.ForeignKey(League)
-    team1 = models.ForeignKey(Team, related_name="Games.team1")
-    team2 = models.ForeignKey(Team, related_name="Games.team2")
+    team1 = models.ForeignKey(Team)
+    team2 = models.ForeignKey(Team)
     game_date = models.DateField(auto_now=False,)
     was_tie = models.BooleanField(default=False)
     was_overtime = models.BooleanField(default=False)
     team1_win = models.BooleanField(default=False)
-    team1_goals = models.IntegerField()
+    team1_goals = models.IntegerField(default=0)
     team2_win = models.BooleanField(default=False)
-    team2_goals = models.IntegerField()
+    team2_goals = models.IntegerField(default=0)
+
+class LeaguePlugin(CMSPlugin):
+    league = models.ForeignKey(League)
