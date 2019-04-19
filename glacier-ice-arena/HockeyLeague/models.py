ef5bd47c87d90ef5fb7b30175c8536c731ab8ee9
DustinLep <DustinLeprechaun@gmail.com>
2019-04-19 11:29:11 -0600

    Update models.py

--- glacier-ice-arena/HockeyLeague/models.py
+++ glacier-ice-arena/HockeyLeague/models.py
@@ -32,7 +32,7 @@ class Player(models.Model):
 
 class Games(models.Model):
     league = models.ForeignKey(League)
-    team1 = models.ForeignKey(Team)
+    team1 = models.ForeignKey(Team, related_name="team1")
     team2 = models.ForeignKey(Team)
     game_date = models.DateField(auto_now=False,)
     was_tie = models.BooleanField(default=False)
