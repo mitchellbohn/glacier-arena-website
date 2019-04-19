3e886c824680a3c0be8ce76946a686a32d968ca4
DustinLep <DustinLeprechaun@gmail.com>
2019-04-19 10:49:31 -0600

    Added plugin reference to Admin and CMS Plugins

---
+++ glacier-ice-arena/HockeyLeague/cms_plugins.py
@@ -0,0 +1,10 @@
+from cms.plugin_pool import plugin_pool
+from cms.plugin_base import CMSPluginBase
+from cms.models.pluginmodel import CMSPlugin
+
+from . import models
+
+class HockeyLeaguePlugin(CMSPluginBase):
+    model = models.League
+    name = 'Hockey League Plugin'
+    render_template = "league.html"
