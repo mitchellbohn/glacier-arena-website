cb901e3cb88606b0ee5ef97fb54c0bfab9a3d402
DustinLep <DustinLeprechaun@gmail.com>
2019-05-09 10:20:40 -0600

    added inline team view to League view

--- glacier-ice-arena/HockeyLeague/admin.py
+++ glacier-ice-arena/HockeyLeague/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(League)
-admin.site.register(Team)
+class TeamsInline(admin.TabularInline):
+    model = Team
+    extra = 0
+
+
+class LeagueAdmin(admin.ModelAdmin):
+    inlines = [TeamsInline,]
+
+admin.site.register(League, LeagueAdmin)
 
--- glacier-ice-arena/HockeyLeague/models.py
+++ glacier-ice-arena/HockeyLeague/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from cms.models import CMSPlugin
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -23,17 +24,22 @@ class Team(models.Model):
     goals_against = models.IntegerField(default=0)
     goal_difference = models.IntegerField(default=0)
     PTS = models.IntegerField(default=0)
+    players = models.ManyToManyField(User, through='Player')
+
+    def __str__(self):
+        return self.name
 
 class Player(models.Model):
+    user = models.ForeignKey(User)
     team = models.ForeignKey(Team)
     name = models.CharField(blank=True, max_length=200,)
-    number = models.IntegerField()
-    position = models.CharField(max_length=100,)
+    number = models.IntegerField(null=True, blank=True,)
+    position = models.CharField(null=True, blank=True, max_length=100,)
 
 class Games(models.Model):
     league = models.ForeignKey(League)
-    team1 = models.ForeignKey(Team, related_name="team1")
-    team2 = models.ForeignKey(Team, related_name="team2")
+    team1 = models.ForeignKey(Team, related_name="home")
+    team2 = models.ForeignKey(Team, related_name="guest")
     game_date = models.DateField(auto_now=False,)
     was_tie = models.BooleanField(default=False)
     was_overtime = models.BooleanField(default=False)
